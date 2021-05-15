86c35361a13cabb0f73bec62d485baa723996afe
fengshaw1 <1196563277@qq.com>
2021-05-15 09:30:27 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import fastClick from "fastclick";
+// 轮播图插件
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/swiper-bundle.css";
+
 // 防止移动端显示出现的问题而引入的css样式文件
 import "styles/reset.css";
 import "styles/border.css";
 import "styles/iconfont.css";
 
 Vue.config.productionTip = false;
-
 // 解决移动端300ms点击延迟问题
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOptions">
+    <swiper-slide v-for="(item, index) in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl"/>
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {
+        // 借助该属性显示轮播小点
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/79faa5a73731e84a7731db49d5baa91e.jpeg'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/c0a60fa20379efa4f02ce527a680dc1b.jpeg'
+      }
+      ]
+    };
+  }
+};
+</script>
+<style lang="stylus" scoped>
+// 穿透样式、将图片轮播小点变成白色
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
