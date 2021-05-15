dc1728a2432a2b3af5d0663a0eef795b6e6b6a75
fengshaw1 <1196563277@qq.com>
2021-05-15 16:43:12 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="(item, index) of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeIcons",
+  data() {
+    return {
+      iconList: [
+        {
+          id: "0001",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/piao.png",
+          desc: "景点门票"
+        },
+        {
+          id: "0002",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "度假"
+        },
+        {
+          id: "0003",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/hotel.png",
+          desc: "酒店"
+        },
+        {
+          id: "0004",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "机票"
+        },
+        {
+          id: "0006",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/package.png",
+          desc: "度假"
+        },
+        {
+          id: "0007",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/hotel.png",
+          desc: "酒店"
+        },
+        {
+          id: "0008",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/flight.png",
+          desc: "机票"
+        },
+        {
+          id: "0009",
+          imgUrl: "//s.qunarzz.com/homenode/images/touchheader/train.png",
+          desc: "火车票"
+        }
+      ]
+    };
+  },
+  // 页码和数据项进行关联的数组算法
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.icons >>> .swiper-container
+  height: 0
+  padding-bottom: 50%
+.icon
+  position: relative
+  overflow: hidden
+  width: 25%
+  height: 0
+  float: left
+  padding-bottom: 25%
+  .icon-img
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: .44rem
+    box-sizing: border-box
+    padding: .1rem
+    .icon-img-content
+      display: block
+      margin: 0 auto
+      height: 100%
+  .icon-desc
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .44rem
+    height: .44rem
+    text-align: center
+    color: $darkTextColor
+    // 借助该css样式实现文字过多出现3个点省略的样式
+    ellipsis()
+</style>
