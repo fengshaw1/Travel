59f9a26b405e460335e5542e9f3ab0feb9a45085
fengshaw1 <1196563277@qq.com>
2021-05-16 16:09:33 +0800

    ajax city

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,24 +1,21 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{ key }}</li>
   </ul>
 </template>
 <script>
 export default {
-  name: "CityAlphabet"
+  name: "CityAlphabet",
+  props: {
+    cities: Object
+  }
 };
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 .list
-  display: flex 
-  flex-direction: column 
+  display: flex
+  flex-direction: column
   justify-content: center
   position: absolute
   top: 1.58rem
